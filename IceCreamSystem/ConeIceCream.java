ee39d5702227c7282afee9fcd486bac4e436fb80
dvaisalij-netizen <dvaisalij@gmail.com>
2025-12-31 08:34:39 +0530

    Add files via upload

---
+++ IceCreamSystem/ConeIceCream.java
@@ -0,0 +1,12 @@
+package IceCreamSystem;
+
+public class ConeIceCream extends IceCream {
+        ConeIceCream(String orderId,int price){
+            this.setOrderId(orderId);
+            this.setPrice(price);
+        }
+        ConeIceCream(){
+
+        }
+}
+
---
+++ IceCreamSystem/CupIceCream.java
@@ -0,0 +1,11 @@
+package IceCreamSystem;
+
+public class CupIceCream extends IceCream {
+        CupIceCream(String orderId,int price){
+            this.setOrderId(orderId);
+            this.setPrice(price);
+        }
+        CupIceCream(){
+        }
+    }
+
